79b90733926c24503a3239cf6d14a7da9d414008
Eldar <eldarpoplauski111@gmail.com>
2022-11-24 04:26:35 +0300

    User args, save/load weights

--- NNClass.py
+++ NNClass.py
@@ -1,91 +1,173 @@
 import copy
 import math
+from pathlib import Path
+import os.path
 from random import randint
-# import numpy as np
 from MatrixClass import Matrix
 
 
 class NN:
     def __init__(self):
-        self.layer1 = None
-        self.layer2 = None
-        self.layer3 = None
-        self.weights1 = None
-        self.weights2 = None
-        self.totalRMS = None
-        self.maxRMS = None
-        self.learning_rate = None
-        self.coeff_of_quantity_of_neurons_second_layer = None
-        self.otherVariables = None
+        self.layer_1: Matrix = None
+        self.layer_2: Matrix = None
+        self.layer_3: Matrix = None
+        self.weights_1: Matrix = None
+        self.weights_2: Matrix = None
+
+        self.otherVariables: dict = {}
+        self.block_size: int = 0
+        self.coeff_quantity_neurons_second_layer: float = 2
+        self.learning_rate: float = 0
+        self.maxRMS: float = 0
+        self.totalRMS: float = 0
+        self.train_iteration_num: int = 0
+        self.loaded_weights: bool = False
+        self.can_load_weights: bool = True
 
     def createNN(self, variables):
         self.otherVariables = variables
-        if self.otherVariables is None:
+        if self.otherVariables == {}:
             return
-        self.totalRMS = 0
-        self.coeff_of_quantity_of_neurons_second_layer = self.otherVariables["coeff_of_quantity_of_neurons_second_layer"]
-        block_size = self.otherVariables["block_width"] * self.otherVariables["block_height"]
-        self.maxRMS = 0.1 * self.coeff_of_quantity_of_neurons_second_layer * block_size * 4
+        # Set variables to create network
+        self.coeff_quantity_neurons_second_layer = self.otherVariables["coeff_quantity_neurons_second_layer"]
+        self.block_size = self.otherVariables["block_width"] * self.otherVariables["block_height"]
+        self.maxRMS = 0.1 * self.coeff_quantity_neurons_second_layer * self.block_size * 4
+        self.can_load_weights = self.otherVariables["can_load_weights"]
+        # Correct formula for calculating maxRMS (use [* 4]s to speed up tests):
+        # self.maxRMS = 0.1 * self.coeff_quantity_neurons_second_layer * self.block_size
         self.learning_rate = self.otherVariables["learning_rate"]
-        # create weights matrices
+        # Create weights matrices
         elements = []
-        for _ in range(block_size * block_size * self.coeff_of_quantity_of_neurons_second_layer):
+        for _ in range(int(self.block_size * self.block_size * self.coeff_quantity_neurons_second_layer)):
             elements.append(randint(int(-1e5), int(1e5)) / 1e5)
-        self.weights1 = Matrix(width=block_size * self.coeff_of_quantity_of_neurons_second_layer, height=block_size, elements=elements)
-        self.weights2 = self.weights1.transpose()
+
+        if self.can_load_weights:
+            self.loadWeights()
+        if not self.loaded_weights:
+            self.weights_1 = Matrix(width=self.block_size * self.coeff_quantity_neurons_second_layer,
+                                        height=self.block_size, elements=elements)
+            self.weights_2 = self.weights_1.transpose()
 
     def trainNN(self, blocks):
-        train_iteration_num = 0
         block_size = len(blocks[0])
 
+        if self.loaded_weights:
+            coeff_Z = (self.block_size * len(blocks)) / \
+                      ((self.block_size + len(blocks)) * self.block_size * self.coeff_quantity_neurons_second_layer + 2)
+            print(f"Compression ratio: {coeff_Z}")
+            return
         while self.totalRMS > self.maxRMS or self.totalRMS == 0:
             self.totalRMS = 0
             for block_index in range(len(blocks)):
-                self.layer1 = Matrix(width=block_size, height=1, elements=blocks[block_index])
-                self.layer2 = self.layer1 * self.weights1
-                self.layer3 = self.layer2 * self.weights2
-                delta_layers13 = self.layer3 - self.layer1
+                # Create layers
+                self.layer_1 = Matrix(width=block_size, height=1, elements=blocks[block_index])
+                self.layer_2 = self.layer_1 * self.weights_1
+                self.layer_3 = self.layer_2 * self.weights_2
+                # Calculate rms and delta between first and last layer
+                delta_layers13: Matrix = self.layer_3 - self.layer_1
                 rms = sum([value * value for value in delta_layers13.getList()])
                 self.totalRMS += rms
-
-                new_weights1 = self.weights1 - (self.layer1.transpose() * delta_layers13 * self.weights2.transpose()) * self.learning_rate
-                new_weights2 = self.weights2 - (self.layer2.transpose() * delta_layers13) * self.learning_rate
-                #print("old-weights1:\n", self.weights1, "old-weights2:\n", self.weights2)
-                self.weights1 = new_weights1
-                self.weights2 = new_weights2
-                self.normalizeWeights(self.weights1)
-                self.normalizeWeights(self.weights2)
-                #print("new-weights1:\n", self.weights1, "new-weights2:\n", self.weights2)
-                train_iteration_num += 1
-
-                print("IT", train_iteration_num, "RMS:", self.totalRMS, "MAX", self.maxRMS)
-
-        # print("layer1: ",self.layer1, "layer2: ", self.layer2, "layer3: ", self.layer3)
-        # print("weights1:\n", self.weights1, "weights2:\n", self.weights2)
+                # Update weights
+                new_weights1 = self.weights_1 - (self.layer_1.transpose() * delta_layers13 * self.weights_2.transpose()) * self.learning_rate
+                new_weights2 = self.weights_2 - (self.layer_2.transpose() * delta_layers13) * self.learning_rate
+                self.weights_1 = new_weights1
+                self.weights_2 = new_weights2
+                self.normalizeWeights(self.weights_1)
+                self.normalizeWeights(self.weights_2)
+                # Output
+                self.train_iteration_num += 1
+                print(f"\rmax error: {self.maxRMS:.{5}f} | "
+                      f"current error: {self.totalRMS:.{5}f} | "
+                      f"Iterations: {self.train_iteration_num}", end="")
+        print("\r")
+        coeff_Z = (self.block_size * len(blocks)) /\
+                  ((self.block_size + len(blocks)) * self.block_size * self.coeff_quantity_neurons_second_layer + 2)
+        print(f"Compression ratio: {coeff_Z}")
 
     def normalizeWeights(self, weights):
         for i in range(0, weights.height):
-            sqsum = 0
+            sum_of_squares = 0
             for k in weights.matrix[i]:
-                sqsum += k * k
-            module = math.sqrt(sqsum)
+                sum_of_squares += k * k
+            module = math.sqrt(sum_of_squares)
             for j in range(0, weights.width):
                 weights.matrix[i][j] /= module
 
     def NNIteration(self, block):
-        self.layer1 = Matrix(width=len(block), height=1, elements=block)
-        self.layer2 = self.layer1 * self.weights1
-        self.layer3 = self.layer2 * self.weights2
-        for index in range(len(self.layer3.matrix[0])):
-            if self.layer3.matrix[0][index] < -1:
-                self.layer3.matrix[0][index] = -1
-            if self.layer3.matrix[0][index] > 1:
-                self.layer3.matrix[0][index] = 1
+        self.layer_1 = Matrix(width=len(block), height=1, elements=block)
+        self.layer_2 = self.layer_1 * self.weights_1
+        self.layer_3 = self.layer_2 * self.weights_2
 
-        return copy.deepcopy(self.layer3.getList())
+        """
+        This function is used to get the final result of the function.
+        Therefore, if numbers appear outside the range from -1 to 1,
+        the value will be converted incorrectly and a prominent artifact
+        will appear in the picture. 
+        To avoid this, we cut off all values that go beyond the limits.
+        """
+        for index in range(len(self.layer_3.matrix[0])):
+            self.layer_3.matrix[0][index] = -1 if self.layer_3.matrix[0][index] < -1 else self.layer_3.matrix[0][index]
+            self.layer_3.matrix[0][index] = 1 if self.layer_3.matrix[0][index] > 1 else self.layer_3.matrix[0][index]
+
+        return copy.deepcopy(self.layer_3.getList())
+
+    def getWeightsFileName(self, weights_index):
+        try:
+            image_name = Path(self.otherVariables['image_name']).stem
+            return f"weights/{image_name}/" \
+                   f"{self.otherVariables['count_of_channels']}_" \
+                   f"{self.otherVariables['block_width']}_" \
+                   f"{self.otherVariables['block_height']}_" \
+                   f"{self.otherVariables['coeff_quantity_neurons_second_layer']}_" \
+                   f"{self.otherVariables['channel_id']}_" \
+                   f"{weights_index}.nnwght"
+        except Exception as ex:
+            print(ex)
 
     def saveWeights(self):
-        print(self.otherVariables["image_name"],
-              self.otherVariables["channel_id"],
-              self.otherVariables["block_width"],
-              self.otherVariables["block_height"])
+        try:
+            image_name = Path(self.otherVariables['image_name']).stem
+            Path(f"weights/{image_name}").mkdir(parents=True, exist_ok=True)
+            with open(self.getWeightsFileName(weights_index=1), "w") as file:
+                for row in self.weights_1.matrix:
+                    string = ""
+                    for x in row:
+                        string += str(x) + " "
+                    file.write(string + "\n")
+            with open(self.getWeightsFileName(weights_index=2), "w") as file:
+                for row in self.weights_2.matrix:
+                    string = ""
+                    for x in row:
+                        string += str(x) + " "
+                    file.write(string + "\n")
+
+        except Exception as ex:
+            print(ex)
+
+    def loadWeights(self):
+        try:
+            file_name_1 = self.getWeightsFileName(weights_index=1)
+            file_name_2 = self.getWeightsFileName(weights_index=2)
+            if os.path.exists(file_name_1) and \
+                    os.path.exists(file_name_2):
+                with open(file_name_1, "r") as file:
+                    elements = []
+                    height = 0
+                    for line in file.readlines():
+                        for x in line[:-2].split(' '):
+                            elements.append(float(x))
+                        height += 1
+                    self.weights_1 = Matrix(width=int(len(elements) / height), height=height, elements=elements)
+                with open(file_name_2, "r") as file:
+                    elements = []
+                    height = 0
+                    for line in file.readlines():
+                        for x in line[:-2].split(' '):
+                            elements.append(float(x))
+                        height += 1
+                    self.weights_2 = Matrix(width=int(len(elements) / height), height=height, elements=elements)
+                self.loaded_weights = True
+            else:
+                print("No weights for this parameters")
+        except Exception as ex:
+            print(ex)
--- main.py
+++ main.py
@@ -5,34 +5,102 @@ from NNClass import NN
 
 def main():
     random.seed()
-    NNVariables = {
-        "image_name": "images/256x256_0.png",
-        "block_width": 1,
-        "block_height": 1,
-        "coeff_of_quantity_of_neurons_second_layer": 2,
-        "quantity_of_blocks_to_train": 1000,
-        "learning_rate": 0.0001
-    }
+    # User input
+    image_file_name = input("Image file name: ")
+    image_file_name = "images/256x256_0.png" \
+        if image_file_name == "" or image_file_name == "\n" else image_file_name
 
+    block_width = input("Block width: ")
+    block_width = 1 \
+        if block_width == "" or block_width == "\n" else int(block_width)
+
+    block_height = input("Block height: ")
+    block_height = 1 \
+        if block_height == "" or block_height == "\n" else int(block_height)
+
+    coeff_quantity_neurons_second_layer = input("Coeff of quantity of neurons on the second layer: ")
+    coeff_quantity_neurons_second_layer = 2 \
+        if coeff_quantity_neurons_second_layer == "" \
+           or coeff_quantity_neurons_second_layer == "\n" else float(coeff_quantity_neurons_second_layer)
+
+    learning_rate = input("Learning rate: ")
+    learning_rate = 0.0001 \
+        if learning_rate == "" or learning_rate == "\n" else float(learning_rate)
+
+    can_load_weights = input("Load saved weights [y/n]: ")
+    can_load_weights = True \
+        if can_load_weights.lower() == "y" or can_load_weights.lower() == "y\n" else False
+
+    # !!!Only for dev always True/False
+    can_load_weights = True
+
+    NNVariables = {
+        "image_name": image_file_name,
+        "block_width": block_width,
+        "block_height": block_height,
+        "coeff_quantity_neurons_second_layer": coeff_quantity_neurons_second_layer,
+        "quantity_of_blocks_to_train": 1000,
+        "learning_rate": learning_rate,
+        "can_load_weights": can_load_weights
+    }
+    print(NNVariables)
+    # Splitting an image into channels and blocks
     image = Image(NNVariables["image_name"])
     image.splitImageChannelsIntoBlocks(NNVariables["block_width"], NNVariables["block_height"])
 
+    """ 
+    It's possible to create only one neural network instead of three(four),
+    for this u just need uniform sampling from all channels of the image
+    """
+
+    """
+    samples = [] 
     for channel in image.channels:
-        # if image.channels.index(channel) != 0:
-        #     break
+        samples += [random.choice(channel.blocks) 
+            for _ in range(0, int(NNVariables["quantity_of_blocks_to_train"] / 3))]
+
+    NNVariables["channel_id"] = 0
+    common_NN = NN()
+    common_NN.createNN(NNVariables)
+    common_NN.trainNN(samples)
+    
+    for channel in image.channels:
+        iterated_blocks = []
+        for block in channel.blocks:
+            iterated_blocks.append(common_NN.NNIteration(block))
+        channel.blocks = iterated_blocks
+    """
+
+    """
+    Three(four) neural networks, one for every channel   
+    with the same error, the quality of the resulting image
+    when using a neural network for each image channel is better
+    than when using a single neural network
+    """
+
+    for channel in image.channels:
+        # Create and train neural network
         channel_NN = NN()
         NNVariables["channel_id"] = image.channels.index(channel)
+        NNVariables["count_of_channels"] = len(image.channels)
         channel_NN.createNN(NNVariables)
-        channel_NN.trainNN([random.choice(channel.blocks) for _ in range(0, NNVariables["quantity_of_blocks_to_train"])])
-
+        # channel_NN.loadWeights()
+        channel_NN.trainNN([random.choice(channel.blocks)
+                            for _ in range(0, NNVariables["quantity_of_blocks_to_train"])])
+        # Run all channel blocks through a neural network
         iterated_blocks = []
         for block in channel.blocks:
             iterated_blocks.append(channel_NN.NNIteration(block))
         channel.blocks = iterated_blocks
+        channel_NN.saveWeights()
 
+    # Restore and save imag
     image.restoreChannelsFromBlocks(NNVariables["block_width"], NNVariables["block_height"])
     image.restoreImageFromChannels()
     image.saveImage()
+
+    # Literature:
+    # https://studfile.net/preview/1557061/page:8/
 
 
 if __name__ == '__main__':
